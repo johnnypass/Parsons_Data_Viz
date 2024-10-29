abf87797730cb461f09ce5e3225c0514e0f19f3d
johnnypass <jwpassmore@gmail.com>
2024-10-29 18:56:26 -0400

    Add files via upload

---
+++ week_4/imdb_lida_test_summary.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+from lida import Manager, llm, TextGenerationConfig  # Replace 'lida_lib' with the actual module name
+
+# Load environment variables from the .env file
+load_dotenv('/Users/johnpassmore/Desktop/openapi.env')
+
+# Get the API key from the environment variables
+api_key = os.getenv('API_KEY')
+
+# Initialize the Manager with the API key
+lida = Manager(text_gen=llm("openai", api_key=api_key))  # Use the API key from the .env file
+textgen_config = TextGenerationConfig(n=1, temperature=0.5, model="gpt-3.5-turbo", use_cache=True)
+
+summary = lida.summarize("/Users/johnpassmore/Desktop/movies.basics.tsv", summary_method="default", textgen_config=textgen_config)
+goals = lida.goals(summary, n=2, textgen_config=textgen_config)
+
+for goal in goals:
+    print(goal)
